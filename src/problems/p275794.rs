f10214044ec29401b643bc4f3ffc76cc9e616b72
8uff3r <8uff3r@gmail.com>
2025-03-04 15:00:43 +0330

    275794: fix some perf issues

--- src/problems/p275794.rs
+++ src/problems/p275794.rs
@@ -6,37 +6,41 @@ pub fn main() {
     input = input.trim_end().to_string();
 
     fn prcess_input(
-        curr: String,
-        acc: usize,
+        mut curr: String,
+        mut acc: usize,
         mut gomrah: usize,
         mut found: usize,
     ) -> (String, usize, usize) {
-        let current_substr: &str = &curr[acc..];
-        let kalan_index = current_substr.find("kalan");
-        if kalan_index.is_none() {
-            (curr, gomrah, found)
-        } else {
-            let kalan_index = kalan_index.unwrap();
-            let tar_index = current_substr[kalan_index + "kalan".len()..].find("tar");
-            if tar_index.is_none() {
-                return (curr, gomrah, found);
-            }
-            let tar_index = tar_index.unwrap() + kalan_index + "kalan".len();
-            let mid_kalantar = &current_substr[(kalan_index + "kalan".len())..(tar_index)];
-            let has_non_space = mid_kalantar.find(|c| c != ' ');
-            if has_non_space.is_some() {
-                return prcess_input(curr, acc + kalan_index + "kalan".len(), gomrah, found);
-            }
-
-            if mid_kalantar.is_empty() {
-                found += 1
+        loop {
+            let current_substr: &str = &curr[acc..];
+            let kalan_index = current_substr.find("kalan");
+            if kalan_index.is_none() {
+                break;
             } else {
-                gomrah += 1
-            }
-            let removed_kalantar =
-                curr[..kalan_index + acc].to_string() + &curr[(tar_index + "tar".len() + acc)..];
-            prcess_input(removed_kalantar.trim().to_string(), acc, gomrah, found)
+                let kalan_index = kalan_index.unwrap();
+                let tar_index = current_substr[kalan_index + "kalan".len()..].find("tar");
+                if tar_index.is_none() {
+                    break;
+                }
+                let tar_index = tar_index.unwrap() + kalan_index + "kalan".len();
+                let mid_kalantar = &current_substr[(kalan_index + "kalan".len())..(tar_index)];
+                let has_non_space = mid_kalantar.find(|c| c != ' ');
+                if has_non_space.is_some() {
+                    acc += kalan_index + "kalan".len();
+                    continue;
+                }
+
+                if mid_kalantar.is_empty() {
+                    found += 1
+                } else {
+                    gomrah += 1
+                }
+                let removed_kalantar = curr[..kalan_index + acc].to_string()
+                    + &curr[(tar_index + "tar".len() + acc)..];
+                curr = removed_kalantar.trim().to_string();
+            };
         }
+        (curr, gomrah, found)
     }
     let mut current_step = input.clone();
     let mut steps_details: Vec<(usize, usize)> = Vec::new();
