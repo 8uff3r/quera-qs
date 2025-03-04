ec732f3e175b7ff31a1ff479e83a846e87d402eb
8uff3r <8uff3r@gmail.com>
2025-03-04 15:05:11 +0330

    remove unneeded nesting

--- src/problems/p275794.rs
+++ src/problems/p275794.rs
@@ -9,38 +9,37 @@ pub fn main() {
         mut curr: String,
         mut acc: usize,
         mut gomrah: usize,
-        mut found: usize,
+        mut tahdid: usize,
     ) -> (String, usize, usize) {
         loop {
             let current_substr: &str = &curr[acc..];
             let kalan_index = current_substr.find("kalan");
             if kalan_index.is_none() {
                 break;
-            } else {
-                let kalan_index = kalan_index.unwrap();
-                let tar_index = current_substr[kalan_index + "kalan".len()..].find("tar");
-                if tar_index.is_none() {
-                    break;
-                }
-                let tar_index = tar_index.unwrap() + kalan_index + "kalan".len();
-                let mid_kalantar = &current_substr[(kalan_index + "kalan".len())..(tar_index)];
-                let has_non_space = mid_kalantar.find(|c| c != ' ');
-                if has_non_space.is_some() {
-                    acc += kalan_index + "kalan".len();
-                    continue;
-                }
+            }
+            let kalan_index = kalan_index.unwrap();
+            let tar_index = current_substr[kalan_index + "kalan".len()..].find("tar");
+            if tar_index.is_none() {
+                break;
+            }
+            let tar_index = tar_index.unwrap() + kalan_index + "kalan".len();
+            let mid_kalantar = &current_substr[(kalan_index + "kalan".len())..(tar_index)];
+            let has_non_space = mid_kalantar.find(|c| c != ' ');
+            if has_non_space.is_some() {
+                acc += kalan_index + "kalan".len();
+                continue;
+            }
 
-                if mid_kalantar.is_empty() {
-                    found += 1
-                } else {
-                    gomrah += 1
-                }
-                let removed_kalantar = curr[..kalan_index + acc].to_string()
-                    + &curr[(tar_index + "tar".len() + acc)..];
-                curr = removed_kalantar.trim().to_string();
-            };
+            if mid_kalantar.is_empty() {
+                tahdid += 1
+            } else {
+                gomrah += 1
+            }
+            let removed_kalantar =
+                curr[..kalan_index + acc].to_string() + &curr[(tar_index + "tar".len() + acc)..];
+            curr = removed_kalantar.trim().to_string();
         }
-        (curr, gomrah, found)
+        (curr, gomrah, tahdid)
     }
     let mut current_step = input.clone();
     let mut steps_details: Vec<(usize, usize)> = Vec::new();
